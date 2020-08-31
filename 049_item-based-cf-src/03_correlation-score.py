48a3903e645172f8fae12a16c83d41d6e3f5afe2
peterhchen <peter.hp.chen@gmail.com>
2020-08-30 18:15:32 -0700

    Add files via upload

---
+++ 049_item-based-cf-src/03_correlation-score.py
@@ -0,0 +1,24 @@
+# Read MovieLens 100K data
+import pandas as pd
+
+r_cols = ['user_id', 'movie_id', 'rating']
+ratings = pd.read_csv('ml-100k/u.data', sep='\t', \
+    names=r_cols, usecols=range(3), encoding="ISO-8859-1")
+
+m_cols = ['movie_id', 'title']
+movies = pd.read_csv('ml-100k/u.item', sep='|', \
+    names=m_cols, usecols=range(2), encoding="ISO-8859-1")
+
+ratings = pd.merge(movies, ratings)
+print('ratings.head():')
+print(ratings.head())
+
+# Construct Matrix
+userRatings = ratings.pivot_table(index=['user_id'],columns=['title'],values='rating')
+print('userRatings.head():')
+print(userRatings.head())
+
+# correlation score
+corrMatrix = userRatings.corr()
+print('corrMatrix.head():')
+print(corrMatrix.head())
