666695b8b943bb6a6f781b975838c339d152726c
peterhchen <peter.hp.chen@gmail.com>
2020-09-26 12:41:45 -0700

    Delete 05_random-normal.py

--- 003_01_python-basic/05_random-normal.py
+++
@@ -1,7 +0,0 @@
-# Import numpy modules (or packages)
-import numpy as np
-# random (class) normal (function)
-# mean = 25.0, deviation = 5.0, 10 points (array index 0 to 9) 
-A = np.random.normal(25.0, 5.0, 10)
-print ('A:')
-print (A)
